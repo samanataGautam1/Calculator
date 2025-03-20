45755014fcaa15dc749afce78e7a42081f33b66d
shreeyaa009 <shionyui6@gmail.com>
2025-03-20 07:58:27 +0545

    add

--- Addition.py
+++ Addition.py
@@ -1,10 +1,2 @@
-def add_numbers():
-    try:
-        num1 = float(input("Enter first number: "))
-        num2 = float(input("Enter second number: "))
-        result = num1 + num2
-        print(f"Result: {result}")
-    except ValueError:
-        print("Invalid input! Please enter numbers only.")
-
-add_numbers()
+def add(x,y):
+    return x+y
